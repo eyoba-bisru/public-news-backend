9c722ddd8e1d2eaf840bf2cc61c15e30bad55dd3
Natnael-M3 <znatnaelm@gmail.com>
2023-01-14 15:09:31 +0300

    configuration crud added

--- src/controllers/configuration.js
+++ src/controllers/configuration.js
@@ -1,59 +1,59 @@
-const { prisma } = require("../../prisma/client/prisma-client");
+const { prisma } = require('../../prisma/client/prisma-client')
 
 async function addContentHandler(req, res) {
-  const { name } = req.body;
+  const { name } = req.body
 
-  if (!name) return res.sendStatus(400);
+  if (!name) return res.sendStatus(400)
 
   try {
     const content = await prisma.content.create({
       data: {
         name,
       },
-    });
+    })
 
-    res.send(content);
+    res.send(content)
   } catch (error) {
-    if (error.code == "P2002")
-      return res.status(400).send("Content already exist");
-    res.sendStatus(500);
+    if (error.code == 'P2002')
+      return res.status(400).send('Content already exist')
+    res.sendStatus(500)
   }
 }
 async function addLanguageHandler(req, res) {
-  const { name } = req.body;
+  const { name } = req.body
 
-  if (!name) return res.sendStatus(400);
+  if (!name) return res.sendStatus(400)
 
   try {
     const language = await prisma.language.create({
       data: {
         name,
       },
-    });
-    res.send(language);
+    })
+    res.send(language)
   } catch (error) {
-    if (error.code == "P2002")
-      return res.status(400).send("Language already exist");
-    res.sendStatus(500);
+    if (error.code == 'P2002')
+      return res.status(400).send('Language already exist')
+    res.sendStatus(500)
   }
 }
 async function addLocationHandler(req, res) {
-  const { name } = req.body;
+  const { name } = req.body
 
-  if (!name) return res.sendStatus(400);
+  if (!name) return res.sendStatus(400)
 
   try {
     const location = await prisma.location.create({
       data: {
         name,
       },
-    });
+    })
 
-    res.send(location);
+    res.send(location)
   } catch (error) {
-    if (error.code == "P2002")
-      return res.status(400).send("Location already exist");
-    res.sendStatus(500);
+    if (error.code == 'P2002')
+      return res.status(400).send('Location already exist')
+    res.sendStatus(500)
   }
 }
 async function sitemapHandler(req, res) {
@@ -63,20 +63,20 @@ async function sitemapHandler(req, res) {
         id: true,
         name: true,
       },
-    });
+    })
     const locations = await prisma.location.findMany({
       select: {
         id: true,
         name: true,
       },
-    });
+    })
 
-    locations.forEach((location) => (location["type"] = "location"));
-    contents.forEach((content) => (content["type"] = "content"));
+    locations.forEach((location) => (location['type'] = 'location'))
+    contents.forEach((content) => (content['type'] = 'content'))
 
-    res.send([...locations, ...contents]);
+    res.send([...locations, ...contents])
   } catch (error) {
-    res.sendStatus(500);
+    res.sendStatus(500)
   }
 }
 
@@ -87,13 +87,13 @@ async function languageHandler(req, res) {
         id: true,
         name: true,
       },
-    });
+    })
 
-    languages.forEach((language) => (language["type"] = "language"));
+    languages.forEach((language) => (language['type'] = 'language'))
 
-    res.send(languages);
+    res.send(languages)
   } catch (error) {
-    res.sendStatus(500);
+    res.sendStatus(500)
   }
 }
 
@@ -104,28 +104,167 @@ async function allHandler(req, res) {
         id: true,
         name: true,
       },
-    });
+    })
 
     const contents = await prisma.content.findMany({
       select: {
         id: true,
         name: true,
       },
-    });
+    })
     const locations = await prisma.location.findMany({
       select: {
         id: true,
         name: true,
       },
-    });
+    })
 
-    languages.forEach((language) => (language["type"] = "language"));
-    locations.forEach((location) => (location["type"] = "location"));
-    contents.forEach((content) => (content["type"] = "content"));
+    languages.forEach((language) => (language['type'] = 'language'))
+    locations.forEach((location) => (location['type'] = 'location'))
+    contents.forEach((content) => (content['type'] = 'content'))
 
-    res.send([...contents, ...languages, ...locations]);
+    res.send([...contents, ...languages, ...locations])
   } catch (error) {
-    res.sendStatus(500);
+    res.sendStatus(500)
+  }
+}
+
+async function contentsHandler(req, res) {
+  try {
+    const contents = await prisma.content.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    })
+
+    contents.forEach((content) => (content['type'] = 'content'))
+
+    res.send(contents)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
+
+async function locationsHandler(req, res) {
+  try {
+    const locations = await prisma.location.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    })
+
+    locations.forEach((location) => (location['type'] = 'location'))
+
+    res.send(locations)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
+
+async function handleDeleteLanguage(req, res) {
+  try {
+    const { id } = req.body
+
+    const deletedLanguage = await prisma.language.delete({
+      where: {
+        id,
+      },
+    })
+
+    res.send(deletedLanguage)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
+
+async function handleDeleteLocation(req, res) {
+  try {
+    const { id } = req.body
+
+    const deletedLocation = await prisma.location.delete({
+      where: {
+        id,
+      },
+    })
+
+    res.send(deletedLocation)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
+
+async function handleDeleteContent(req, res) {
+  try {
+    const { id } = req.body
+
+    const deletedContent = await prisma.content.delete({
+      where: {
+        id,
+      },
+    })
+
+    res.send(deletedContent)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
+
+async function handleEditContent(req, res) {
+  try {
+    const { id, name } = req.body
+
+    const updatedContent = await prisma.content.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+      },
+    })
+
+    res.send(updatedContent)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
+
+async function handleEditLocation(req, res) {
+  try {
+    const { id, name } = req.body
+
+    const updatedLocation = await prisma.location.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+      },
+    })
+
+    res.send(updatedLocation)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
+
+async function handleEditLanguage(req, res) {
+  try {
+    const { id, name } = req.body
+
+    const updatedLanguage = await prisma.language.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+      },
+    })
+
+    res.send(updatedLanguage)
+  } catch (error) {
+    res.sendStatus(500)
   }
 }
 
@@ -136,4 +275,12 @@ module.exports = {
   sitemapHandler,
   languageHandler,
   allHandler,
-};
+  contentsHandler,
+  locationsHandler,
+  handleDeleteContent,
+  handleDeleteLanguage,
+  handleDeleteLocation,
+  handleEditContent,
+  handleEditLanguage,
+  handleEditLocation,
+}
--- src/routes/configuration.js
+++ src/routes/configuration.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express')
 const {
   addContentHandler,
   addLanguageHandler,
@@ -6,15 +6,31 @@ const {
   sitemapHandler,
   languageHandler,
   allHandler,
-} = require("../controllers/configuration");
+  locationsHandler,
+  contentsHandler,
+  handleDeleteContent,
+  handleDeleteLanguage,
+  handleDeleteLocation,
+  handleEditLocation,
+  handleEditLanguage,
+  handleEditContent,
+} = require('../controllers/configuration')
 // const { requireUser } = require("../middlewares/requireUser");
-const router = express.Router();
+const router = express.Router()
 
-router.post("/addLocation", addLocationHandler);
-router.post("/addContent", addContentHandler);
-router.post("/addLanguage", addLanguageHandler);
-router.get("/sitemap", sitemapHandler);
-router.get("/languages", languageHandler);
-router.get("/all", allHandler);
+router.post('/addLocation', addLocationHandler)
+router.post('/addContent', addContentHandler)
+router.post('/addLanguage', addLanguageHandler)
+router.get('/sitemap', sitemapHandler)
+router.get('/languages', languageHandler)
+router.get('/contents', contentsHandler)
+router.get('/locations', locationsHandler)
+router.get('/all', allHandler)
+router.delete('/content', handleDeleteContent)
+router.delete('/location', handleDeleteLocation)
+router.delete('/language', handleDeleteLanguage)
+router.patch('/language', handleEditLanguage)
+router.patch('/content', handleEditContent)
+router.patch('/location', handleEditLocation)
 
-module.exports = router;
+module.exports = router
