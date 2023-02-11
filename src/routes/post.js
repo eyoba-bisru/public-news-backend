38127f56ca89c7bdfb8e8e15b799916950710065
Natnael-M3 <znatnaelm@gmail.com>
2023-02-11 15:55:18 +0300

    delete comment

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const fileUpload = require("express-fileupload");
+const express = require('express')
+const fileUpload = require('express-fileupload')
 const {
   postsHomeHandler,
   addPostHandler,
@@ -34,62 +34,65 @@ const {
   loadMoreLocationHandler,
   deletePostHandler,
   handleEditPost,
-} = require("../controllers/post");
-const fileExtLimiter = require("../middlewares/fileExtLimiter");
-const fileSizeLimiter = require("../middlewares/fileSizeLimiter");
-const filesPayloadExists = require("../middlewares/filesPayloadExists");
-const filesPayloadNotExists = require("../middlewares/filesPayloadNotExists");
-const { requireUser } = require("../middlewares/requireUser");
-const router = express.Router();
+  deleteCommentHandler,
+} = require('../controllers/post')
+const fileExtLimiter = require('../middlewares/fileExtLimiter')
+const fileSizeLimiter = require('../middlewares/fileSizeLimiter')
+const filesPayloadExists = require('../middlewares/filesPayloadExists')
+const filesPayloadNotExists = require('../middlewares/filesPayloadNotExists')
+const { requireUser } = require('../middlewares/requireUser')
+const router = express.Router()
 
-router.post("/", postHandler);
-router.get("/postsHome", postsHomeHandler);
+router.post('/', postHandler)
+router.get('/postsHome', postsHomeHandler)
 router.post(
-  "/addPost",
+  '/addPost',
   requireUser,
   fileUpload({ createParentPath: true }),
   filesPayloadExists,
-  fileExtLimiter([".png", ".jpg", ".jpeg"]),
+  fileExtLimiter(['.png', '.jpg', '.jpeg']),
   fileSizeLimiter,
   addPostHandler
-);
+)
 
-router.get("/reportFetch", reportFetchHandler);
-router.delete("/reportDelete", reportDeleteHandler);
-router.post("/reports", reportsHandler);
-router.post("/loadMore", loadMoreHandler);
-router.post("/loadMoreLanguage", loadMoreLanguageHandler);
-router.post("/loadMoreLocation", loadMoreLocationHandler);
-router.post("/postsCategory", postsCategoryHandler);
-router.post("/recommended", recommendedHandler);
-router.post("/contentPosts", contentPostsHandler);
-router.post("/languagePosts", languagePostsHandler);
-router.post("/locationPosts", locationPostsHandler);
-router.post("/bookmark", bookmarkHandler);
-router.post("/removeBookmark", removeBookmarkHandler);
-router.post("/bookmarks", bookmarksHandler);
-router.get("/numOfPosts", numOfPostsHandler);
-router.get("/postedNews", requireUser, postedNewsHandler);
-router.post("/like", likeHandler);
-router.post("/unlike", unlikeHandler);
-router.post("/isLiked", isLikedHandler);
-router.post("/isUnliked", isUnlikedHandler);
-router.post("/numOfLiked", numOfLikedHandler);
-router.post("/comments", commentsHandler);
-router.post("/getAllComments", getAllCommentsHandler);
-router.post("/customize", customizeHandler);
-router.post("/search", searchHandler);
-router.get("/analytics", analyticsHandler);
-router.post("/addVistor", addVistorHandler);
-router.get("/visitors", visitorsHandler);
-router.delete("/deletePost", deletePostHandler);
+router.get('/reportFetch', reportFetchHandler)
+router.delete('/reportDelete', reportDeleteHandler)
+router.post('/reports', reportsHandler)
+router.post('/loadMore', loadMoreHandler)
+router.post('/loadMoreLanguage', loadMoreLanguageHandler)
+router.post('/loadMoreLocation', loadMoreLocationHandler)
+router.post('/postsCategory', postsCategoryHandler)
+router.post('/recommended', recommendedHandler)
+router.post('/contentPosts', contentPostsHandler)
+router.post('/languagePosts', languagePostsHandler)
+router.post('/locationPosts', locationPostsHandler)
+router.post('/bookmark', bookmarkHandler)
+router.post('/removeBookmark', removeBookmarkHandler)
+router.post('/bookmarks', bookmarksHandler)
+router.get('/numOfPosts', numOfPostsHandler)
+router.get('/postedNews', requireUser, postedNewsHandler)
+router.post('/like', likeHandler)
+router.post('/unlike', unlikeHandler)
+router.post('/isLiked', isLikedHandler)
+router.post('/isUnliked', isUnlikedHandler)
+router.post('/numOfLiked', numOfLikedHandler)
+router.post('/comments', commentsHandler)
+router.post('/getAllComments', getAllCommentsHandler)
+router.post('/customize', customizeHandler)
+router.post('/search', searchHandler)
+router.get('/analytics', analyticsHandler)
+router.post('/addVistor', addVistorHandler)
+router.get('/visitors', visitorsHandler)
+router.delete('/deletePost', deletePostHandler)
 router.patch(
-  "/editPost",
+  '/editPost',
   requireUser,
   fileUpload({ createParentPath: true }),
   filesPayloadNotExists,
-  fileExtLimiter([".png", ".jpg", ".jpeg"]),
+  fileExtLimiter(['.png', '.jpg', '.jpeg']),
   fileSizeLimiter,
   handleEditPost
-);
-module.exports = router;
+)
+router.delete('/deleteComment', deleteCommentHandler)
+
+module.exports = router
